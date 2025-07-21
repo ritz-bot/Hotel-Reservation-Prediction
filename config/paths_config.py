8061d8312a6bee46fd2dca918a00a72c90565c06
Ridham <ridhampuri42@gmail.com>
2025-07-21 18:40:55 +0200

    changes in training_pipeline  and paths

--- config/paths_config.py
+++ config/paths_config.py
@@ -1,3 +1,4 @@
+'''
 import os
 
 
@@ -27,3 +28,31 @@ PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR, "processed_test.csv")
 ###################### MODEL TRAINING ###########################
 
 MODEL_OUTPUT_PATH = "artifacts/models/lgbm_model.pkl"
+'''
+
+import os
+
+# Get the project root directory
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+########### DATA INGESTION ##############
+
+# where to store data after ingestion
+RAW_DIR = os.path.join(ROOT_DIR, "artifacts/raw")
+RAW_FILE_PATH = os.path.join(RAW_DIR, "raw.csv")
+TRAIN_FILE_PATH = os.path.join(RAW_DIR, "train.csv")
+TEST_FILE_PATH = os.path.join(RAW_DIR, "test.csv")
+
+CONFIG_PATH = os.path.join(ROOT_DIR, "config", "config.yaml")
+
+################### DATA PROCESSING ######################
+
+# HERE ALL PATHS REQUIRED FOR DATA PROCESSING ARE DEFINED
+
+PROCESSED_DIR = os.path.join(ROOT_DIR, "artifacts/processed")
+PROCESSED_TRAIN_DATA_PATH = os.path.join(PROCESSED_DIR, "processed_train.csv")
+PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR, "processed_test.csv")
+
+###################### MODEL TRAINING ###########################
+
+MODEL_OUTPUT_PATH = os.path.join(ROOT_DIR, "artifacts/models/lgbm_model.pkl")
